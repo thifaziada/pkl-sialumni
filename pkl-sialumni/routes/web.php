737f9a22072c3a9ca06ec117fa9737ca5c0a4a11
Thifa Ziada <thifazia13@gmail.com>
2024-02-06 22:03:30 +0700

    progress 1

--- pkl-sialumni/routes/web.php
+++ pkl-sialumni/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/landing', [HomeController::class, 'index']);
 
 /**
  * socialite auth
